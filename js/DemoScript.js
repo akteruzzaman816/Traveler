782a86becc72eeaf6184e89f1011faccc19714ea
AkterUzzaman <akteruzzaman816@gmail.com>
2019-05-24 22:46:06 +0600

    all file uploaded

---
+++ js/DemoScript.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  //autocomplete Tags
+  // const searchAutoComplete=document.querySelector('.autocomplete');
+  // M.autocomplete.init(searchAutoComplete,{
+  //   data:{
+  //     "Bangladesh": null,
+  //     "India": null,
+  //     "Pakistan": null,
+  //     "Nepal": null,
+  //     "Vutan": null,
+  //     "China": null,
+  //     "Japan": null,
+  //     "Korea": null,
+  //     "USA": null,
+  //     "Australia": null,
+  //     "Maldives": null,
+  //     "South-Africa": null,
+  //     "Poland": null,
+  //     "Egypt": null,
+  //     "Candada": null,
+  //     "Holand": null,
+  //     "Barmuda": null,
+  //   }
+  // });
+   $('.sidenav').sidenav();
+   $('.slider').slider({indicators:true,height:550});
+
+   // material box
+   $('.materialboxed').materialbox();
+
+
+
+ });
